bfe62da438f719cce5f5414110265858daefbd00
genaki <web.artisant@gmail.com>
2020-07-20 22:45:30 +0500

    Add migration, model and seeder for product

---
+++ database/migrations/2020_07_20_115028_create_product_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('name', 255);
+            $table->decimal('price', 15, 2);
+            $table->text('description')->nullable();
+            $table->string('preview_img', 255)->nullable();
+            $table->string('full_img', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
