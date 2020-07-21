388d8d3eadf1c130e9456b327e87aa88b0f175bb
genaki <web.artisant@gmail.com>
2020-07-21 12:20:31 +0500

    Add preview for product

--- resources/js/components/Product.vue
+++ resources/js/components/Product.vue
@@ -3,56 +3,99 @@
 
         <v-card-text>
             <v-img
+                @click="dialog = true"
                 :src="item.preview_img"
                 lazy-src="/storage/lazy.svg"
                 max-height="240"
                 height="240"
                 contain>
                 <v-btn
+                    @click="dialog = true"
                     class="product__btn-detail"
-                    v-bind="attrs"
-                    v-on="on"
                     fab small absolute bottom right depressed
-
-                >
-                    <v-icon dark>mdi-dots-vertical</v-icon>
-                </v-btn>
+                ><v-icon dark>mdi-dots-vertical</v-icon></v-btn>
             </v-img>
             <v-card-title class="p-0">{{item.name}}</v-card-title>
             <div class="desc">{{item.description}}</div>
-            <!--            <div class="my-4 subtitle-1">-->
-            <!--                <v-row align="center">-->
-            <!--                    <v-col cols="8"></v-col>-->
-            <!--                    <v-col cols="4" class="d-flex">-->
-            <!--                        <v-text-field outlined dense hide-details type="number" min="1" value="1"></v-text-field>-->
-            <!--                    </v-col>-->
-            <!--                </v-row>-->
-            <!--            </div>-->
-
-            <!--            <v-card-actions class="d-flex justify-center">-->
-            <!--                -->
-            <!--            </v-card-actions>-->
         </v-card-text>
         <v-card-text class="d-flex align-center pt-0">
-            <span class="product__price deep-orange--text">${{item.price}}</span>
+            <span class="product__price deep-orange--text" v-html="this.format_price"></span>
             <v-spacer></v-spacer>
             <v-btn depressed rounded outlined color="deep-orange lighten-1" class="product__btn-to-basket">
                 Add to cart
             </v-btn>
         </v-card-text>
-        <v-dialog
-            v-model="dialog"
-            width="500"
-        ></v-dialog>
+
+        <v-dialog v-model="dialog" max-width="900">
+            <v-card class="product-detail">
+                <v-btn
+                    @click="dialog = false"
+                    class="product-detail__btn-close"
+                    text
+                    fab small absolute top right depressed
+                >
+                    <v-icon dark>mdi-close</v-icon>
+                </v-btn>
+                <v-row class="p-md-5" no-gutters>
+                    <v-col cols="12" sm="6" md="7">
+                        <v-img
+                            :src="item.full_img"
+                            lazy-src="/storage/lazy.svg"
+                            contain>
+                        </v-img>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="5" class="d-flex flex-column justify-center">
+                        <v-card-title class="headline">{{item.name}}</v-card-title>
+                        <v-card-text>
+                            <div class="product__price deep-orange--text" v-html="this.format_price"></div>
+                            <div class="mt-2">{{item.description}}</div>
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-row no-gutters>
+                                <v-col cols="3" class="d-flex flex-column justify-center">
+                                    <v-text-field
+                                        class="mr-2"
+                                        type="number"
+                                        min="1"
+                                        value="1"
+                                        outlined
+                                        dense
+                                        hide-details
+                                    ></v-text-field>
+                                </v-col>
+                                <v-col cols="9" class="d-flex flex-column justify-center">
+                                    <v-btn depressed rounded color="deep-orange lighten-1"
+                                           class="product__btn-to-basket">
+                                        <span class="white--text">Add to cart</span>
+                                    </v-btn>
+                                </v-col>
+                            </v-row>
+                        </v-card-actions>
+                    </v-col>
+                </v-row>
+            </v-card>
+        </v-dialog>
     </v-card>
 </template>
 
 <script>
     export default {
         props: ['item'],
-        data () {
+        data() {
             return {
                 dialog: false
+            }
+        },
+        computed: {
+            format_price() {
+                let price = parseFloat(this.item.price),
+                    arPrice = price.toString().split('.');
+
+                if (arPrice.length === 2) {
+                    return `${arPrice[0]}<sup>${arPrice[1]}</sup>`;
+                }
+
+                return '$' + price;
             }
         }
     }
