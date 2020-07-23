0d5135c8eb4cbbe1ca89cce0309c9a33d9016cf9
genaki <web.artisant@gmail.com>
2020-07-23 21:05:07 +0500

    Set name to route for order and cart index pages

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PageController@index')->name('main');
 
 Route::prefix('cart')->group(static function () {
-    Route::get('/', 'CartController@index');
+    Route::get('/', 'CartController@index')->name('cart');
     Route::get('add_item/{product}', 'CartController@addItem');
     Route::get('delete_item/{product}', 'CartController@deleteItem');
     Route::get('change_quantity/{product}', 'CartController@changeQuantity');
 });
 
 Route::group(['prefix' => 'order',  'middleware' => 'order'], static function () {
-    Route::get('/', 'OrderController@index');
+    Route::get('/', 'OrderController@index')->name('order');
     Route::get('confirm', 'OrderController@confirm');
 });
